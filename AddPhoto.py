a4208675eec3c75894c8c7340dd42087b42a4f96
Dylan Tomei <dtomei13@gmail.com>
2020-09-06 22:29:43 -0400

    Add upload
    Added ability to browse computer

--- AddPhoto.py
+++ AddPhoto.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
 from PIL import Image, ImageTk
 
 
@@ -15,6 +17,12 @@ def window(main):
     y = (hs/2) - (h/2)
     main.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
+fileName = None
+
+def uploadFile():
+   filename = filedialog.askopenfilename(initialdir = "/", title = "Select Image")
+   fileName = "yo"
+
 
 
 
@@ -27,21 +35,24 @@ def showWindow():
     textVar = StringVar()
    
 
-    uploadLabel = Label(root, text = 'File Path', textvariable = textVar)
+    uploadLabel = Label(root, text = "File Path", textvariable = textVar)
     uploadLabel.place(x = 75, y = 50)
     
-
-    uploadPath = Entry(root, width = 20)
+    uploadPath = Label(root,text = fileName, width = 20)
     uploadPath.place(x = 150, y = 50)
+   
     
 
-    uploadButton = Button(root, text = 'Upload Image')
+    uploadButton = Button(root, text = 'Upload Image', command=uploadFile)
     uploadButton.place(x = 100, y = 150)
 
     cancelButton = Button(root, text = 'Cancel', command=root.destroy)
     cancelButton.place(x = 350, y = 150)
 
     window(root)
-    
+    root.update
+   
+
+
 
 
